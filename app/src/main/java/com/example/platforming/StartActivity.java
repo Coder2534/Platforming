25514053eae36bd7b94823d6fa38fb1b018597d9
김민창 <ssak2534@gmail.com>
2022-03-04 14:54:32 +0900

    seperate LoginActivity add fragment

---
+++ app/src/main/java/com/example/platforming/StartActivity.java
@@ -0,0 +1,37 @@
+package com.example.platforming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+    Handler handler_splash;
+    Runnable runnalbe_splash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        runnalbe_splash = new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(StartActivity.this, SignInActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }};
+
+        handler_splash = new Handler();
+        handler_splash.postDelayed(runnalbe_splash, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler_splash.removeCallbacks(runnalbe_splash);
+    }
+}
