cc76d3cafb09e3e9a24e53d3fcc46233f6a2d03f
김민창 <ssak2534@gmail.com>
2022-06-23 10:56:43 +0900

    2022-06-21

--- app/src/main/java/com/android/platforming/activity/MainActivity.java
+++ app/src/main/java/com/android/platforming/activity/MainActivity.java
@@ -1,15 +1,22 @@
 package com.android.platforming.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.android.platforming.fragment.MainPageFragment;
 import com.android.platforming.object.User;
 import com.example.platforming.R;
 import com.android.platforming.fragment.UserInitialSettingFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,35 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             getSupportFragmentManager().beginTransaction().replace(R.id.cl_main, new MainPageFragment());
+
+            Toolbar toolbar = findViewById(R.id.tb_main);
+            setSupportActionBar(toolbar);
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 왼쪽 상단 버튼 만들기
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24); //왼쪽 상단 버튼 아이콘 지정
+
+            drawerLayout = (DrawerLayout)findViewById(R.id.dl_main);
+            navigationView = (NavigationView)findViewById(R.id.nv_main);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() { //뒤로가기 했을 때
+        if (drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
