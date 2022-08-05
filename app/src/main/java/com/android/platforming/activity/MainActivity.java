806365e7773408988c11517792db374718d4feeb
user <qpalyfe@gmail.com>
2022-08-05 23:52:49 +0900

    .

--- app/src/main/java/com/android/platforming/activity/MainActivity.java
+++ app/src/main/java/com/android/platforming/activity/MainActivity.java
@@ -19,6 +19,7 @@ import com.android.platforming.clazz.ExpandableList;
 import com.android.platforming.clazz.NotificationHelper;
 import com.android.platforming.fragment.MainPageFragment;
 import com.android.platforming.clazz.User;
+import com.android.platforming.fragment.SchoolmealFragment;
 import com.example.platforming.R;
 import com.android.platforming.fragment.UserInitialSettingFragment;
 import com.google.android.material.navigation.NavigationView;
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         mainExpandableList.addParent("학교 정보", R.drawable.ic_baseline_school_24);
         mainExpandableList.addChild(1, "학교소개", new Fragment());
         mainExpandableList.addChild(1, "전화번호", new Fragment());
-        mainExpandableList.addChild(1, "식단표", new Fragment());
+        mainExpandableList.addChild(1, "식단표", new SchoolmealFragment());
         mainExpandableList.addChild(1, "학사일정", new Fragment());
 
         mainExpandableList.addParent("게시판", R.drawable.ic_baseline_format_list_bulleted_24);
--- app/src/main/java/com/android/platforming/clazz/SchoolApi.java
+++ app/src/main/java/com/android/platforming/clazz/SchoolApi.java
@@ -1,4 +1,75 @@
 package com.android.platforming.clazz;
 
-public class SchoolApi {
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
+public class SchoolApi extends Thread {
+    List result;
+    String calenerTime = ;
+    String key = "key=abfda37c182d43bb9fa4a7e698b91fbb";
+    String Type = "&Type=json";
+    String base_url = "https://open.neis.go.kr/hub/mealServiceDietInfo?";
+    String sub_url = "&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750447&MLSV_YMD="+calenerTime;
+    String result_URL;
+
+    public void schoolApi(int Time){
+        calenerTime = String.valueOf(Time);
+        result_URL = base_url + key + Type+"&Plndex=1&pSize=10"+ sub_url;
+    }
+
+
+
+
+    @Override
+    public void run() {
+        Log.d("result_URL_School", result_URL);
+        try {
+            URL url = new URL(result_URL);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            if (conn != null){
+                conn.connect();
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+
+
+                int recode = conn.getResponseCode();
+                int HTTP_OK = HttpURLConnection.HTTP_OK;
+                Log.d("json_School","recode : "+ recode);
+                Log.d("json_School","HTTp_OK : "+ HTTP_OK);
+
+                if(recode == HTTP_OK){
+                    BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                    String line;
+
+                    while (true){
+                        line = bf.readLine();
+
+                        if(line == null){
+                            break;
+                        }
+                        result = JsonParser.JsonParser(line);
+                        if(result == null){
+                            result = Collections.singletonList("등록된 식단이 없습니다.");
+                        }
+                        Log.d("result_School", String.valueOf(result));
+                    }
+                    bf.close();
+                }
+                conn.disconnect();
+            }
+        }catch (Exception e) {
+            Log.d("error_School",e.getMessage());
+        }
+    }
+    public List<String> getResult(){
+        return result;
+    }
+
 }
