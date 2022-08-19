43f5dbf6db96f7b5aade9c0c914a465bd3c1a99c
lkaru <qpalyfe@gmail.com>
2022-08-19 15:15:19 +0900

    push

---
+++ app/src/main/java/com/android/platforming/fragment/PointStoreFragment.java
@@ -0,0 +1,25 @@
+package com.android.platforming.fragment;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.platforming.R;
+
+public class PointStoreFragment extends Fragment {
+    Dialog profiledialog;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pointstore, container, false);
+
+        profiledialog = new Dialog(getContext());
+    return view;
+    }
+}
