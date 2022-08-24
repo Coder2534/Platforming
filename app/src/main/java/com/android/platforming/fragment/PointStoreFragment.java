20f03aeb4d4b80ef0bb7a4cefa41225d766e57ea
user <qpalyfe@gmail.com>
2022-08-25 00:23:26 +0900

    .

--- app/src/main/java/com/android/platforming/fragment/PointStoreFragment.java
+++ app/src/main/java/com/android/platforming/fragment/PointStoreFragment.java
@@ -193,7 +193,6 @@ public class PointStoreFragment extends Fragment {
         btn_pointstore_savefont = fontdialog.findViewById(R.id.btn_pointstore_savefont);
         et_pointstore_testtext = fontdialog.findViewById(R.id.et_pointstore_testtext);
         tv_pointstore_point = fontdialog.findViewById(R.id.tv_pointstore_font_point);
-
     }
     private void setThemedialogView(){
         tv_pointstore_theme_point = themedialog.findViewById(R.id.tv_pointstore_theme_point);
